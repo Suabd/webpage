0aef948aae4d8e0022b583cc8b2ad15c44f1bdfe
Your Name <you@example.com>
2023-02-19 18:39:41 -0600

    loop

---
+++ lab week 5/loops.js
@@ -0,0 +1,12 @@
+let birds = ['Owl', 'Robin', 'Eagle']
+console.log(birds)
+
+birds.forEach( function(bird: string , index:number ){
+    console.log(bird.toUpperCase())
+})
+
+// TRADITIONAL FOR LOOP
+for (let x = 0 ; x < birds.lemgth ; x ++) {
+    let bird = birds[x]
+    console.log(birds.toUpperCase())
+}
