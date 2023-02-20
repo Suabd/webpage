6d13f5e76740f0629795eaf4f853d87d08c648e6
Your Name <you@example.com>
2023-02-19 18:39:56 -0600

    conditions

---
+++ lab week 5/conditions.js
@@ -0,0 +1,17 @@
+let takenCSharp = false
+let takenJava = true
+if (takenCSharp || takenJava) {
+    console.log('You meet the pre-requisite for Android')
+} else {
+    console.log('You must take C# or Java')
+}
+ let age = 35
+ let usCitizenTime = 9
+ let stateOfResidence = 'Wisconsin'
+ let stateWantRepresent = 'Minnesota'
+
+if (age>= 30 && usCitizenTime >=9 && stateOfResidence == stateWantRepresent){
+    console.log('You are eligible to be a sentor')
+} else {
+    console.log('Sorry, you are not eligible')
+}
