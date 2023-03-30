1993061060647c5ea8e4c6381357d7f138f362f6
Your Name <you@example.com>
2023-03-30 18:38:54 -0500

    update

--- lab week 6/bridges_lab/map.js
+++ lab week 6/bridges_lab/map.js
@@ -17,23 +17,55 @@ let bridgeIcon = L.icon({
     
 });
 
+let goldenIcon = L.icon({
+    iconUrl: 'golden.png',
+    iconAnchor: [30, 30],     // move the icon so the bottom of the pin is on top of the coordiates
+    iconSize: [30, 30],       // define size 
+    popupAnchor: [-15, -25]   // where is popup, relative to icon
+    
+});
+
 
 bridges =  [
-    {"name": "Verrazano-Narrows Bridge	",  "coordinates": [40.6066, -74.0447] }, 
-    {"name": "Golden Gate Bridge",  "coordinates": [37.8199, -122.4783] }, 
-    {"name": "Mackinac Bridge	",  "coordinates": [45.8174, -84.7278] }, 
-    {"name": "George Washington Bridge",  "coordinates": [40.8517, -73.9527] }, 
-    {"name": "Tacoma Narrows Bridge",  "coordinates": [47.2690, -122.5517 ] }
+    {"name": "Verrazano-Narrows Bridge	", "span" : 1298.4, "coordinates": [40.6066, -74.0447] }, 
+    {"name": "Golden Gate Bridge", "span": 1280.2,  "coordinates": [37.8199, -122.4783] }, 
+    {"name": "Mackinac Bridge	","span": 1158.0,  "coordinates": [45.8174, -84.7278] }, 
+    {"name": "George Washington Bridge", "span": 1067.0, "coordinates": [40.8517, -73.9527] }, 
+    {"name": "Tacoma Narrows Bridge", "span": 853.44,  "coordinates": [47.2690, -122.5517 ] }
+
 ]
 
+// adding bridge marker at the bridge locations
 bridges.forEach(function(usaBridges){
-    // draw a marker for this college compus
-    let markerText = `${usaBridges.name}`
+    // draw a marker for this bridge 
+    let markerText = usaBridges.name
     L.marker(usaBridges.coordinates, {icon: bridgeIcon} ).bindPopup (markerText).addTo(map);
    
 });
 
+
+// adds diffrent bridge icon marker for the longest brisge
+let bridgeCoordinates = [40.6066, -74.0447]
+let bridgeMarker = L.marker(bridgeCoordinates, {icon: goldenIcon} ).addTo(map);
+
 //L.marker([51.5, -0.09], {icon: bridgeIcon}).addTo(map);
+
+
+// 
+let usaBridgNameArray = []
+
+bridges.forEach(function(bridgeObject){
+    let bridgeName = bridgeObject.name  // String
+    usaBridgNameArray.push(bridgeName)
+})
+
+let usaBridgesSpanArray = []
+bridges.forEach(function(spanObject){
+    let spanData = spanObject.span  //String
+    usaBridgesSpanArray.push(spanData)
+
+})
+    
 
 let canvas = document.querySelector('#bridges-chart')
 let context = canvas.getContext('2d')
@@ -41,11 +73,11 @@ let context = canvas.getContext('2d')
 chart = new Chart(context, { 
     type: 'bar',
     data: {
-        labels: ["Verrazano-Narrows Bridge", "Golden Gate Bridge", "Mackinac Bridge	", "George Washington Bridge","Tacoma Narrows Bridge"],  
+        labels: usaBridgNameArray,  
         datasets: [{
             label: 'USA bridges Chart',
-            data: [1298.4, 1280.2, 1158.0, 1067.0, 853.44],   // this is the chart data
-            backgroundColor: ['red', 'blue', 'yellow', 'green', 'purple'] // the colors of the data
+            data: usaBridgesSpanArray ,   // this is the chart data
+            backgroundColor: ['red', 'blue', 'yellow', 'green', 'purple'] ,// the colors of the data
         }]
     }, options: {
         scales: {
